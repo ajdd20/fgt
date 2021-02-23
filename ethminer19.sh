532c6c71cb6a2835f342b2db56cbbdf4f838442c
eko772 <79356217+eko772@users.noreply.github.com>
2021-02-23 12:27:15 +0700

    Add files via upload

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x05a10aaa1e862c786a2beada3ca072f5cd38e02f.ab1@eu1.ethermine.org:4444
